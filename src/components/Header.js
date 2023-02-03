c174a386b6def2b57852f5f51c2f64b407e214b3
El Brewster <brewsterel3@gmail.com>
2023-02-02 17:09:07 -0800

    Add assets file and some header and footer styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import "../css/Header.scss";
+import logo from "../assets/logo.svg";
+
 
 export default function Header () {
     return (
         <header>
             <div>
-                <img alt="logo"/>
+                <img src={logo} alt="logo"/>
                 <h1>Rancid Tomatillos</h1>
             </div>
         </header>

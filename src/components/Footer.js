affd7a6eafcfa3326f5e67aca1ff837adeef33bf
El Brewster <brewsterel3@gmail.com>
2023-02-02 19:26:52 -0800

    Add color pallete grid

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import "../css/Footer.scss";
+import MiniPalette from "./MiniPalette";
 
 export default function Footer () {
     return (
         <footer>
             <p>Our Git Hub Tags</p>
+            <MiniPalette />
         </footer>
     )
 }

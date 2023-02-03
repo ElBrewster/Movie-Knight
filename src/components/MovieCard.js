11e394a3f125941c7cf006972c0819383ef93f17
El Brewster <brewsterel3@gmail.com>
2023-02-02 17:23:19 -0800

    Add movie card file and some implementation

---
+++ src/components/MovieCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/SingleMovie.scss";
+
+export default function MovieCard () {
+    return (
+        <h2>movie card</h2>
+    );
+}
--- src/components/MovieCardsContainer.js
+++ src/components/MovieCardsContainer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../css/MovieCardsContainer.scss'
+import MovieCard from './MovieCard';
 
 export default function MovieCardsContainer() {
     return (
         <>
             <section className='movie-card-overview'>
-                hello where is Nemo 
+                hello where is Nemo? is he under the bus? did we throw him under the bus???
+                <MovieCard />
             </section>
         </>
     )

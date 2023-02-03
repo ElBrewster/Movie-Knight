e6e19541988579d5a5f8a032a37cf5b9ffe116ad
El Brewster <brewsterel3@gmail.com>
2023-02-02 17:00:08 -0800

    Add header and footer element components for our app component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "../css/Header.scss";
+
+export default function Header () {
+    return (
+        <header>
+            <div>
+                <img alt="logo"/>
+                <h1>Rancid Tomatillos</h1>
+            </div>
+        </header>
+    );
+}

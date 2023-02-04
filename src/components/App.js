5f8672b9d01b3901249f750f7106eabef74a28b8
Karrar Qasim <Karawr1996@gmail.com>
2023-02-04 12:12:13 -0700

    Removed app at scss from app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
-// import logo from './logo.svg';
 import React from 'react'
 import AllMovies from './AllMovies'
-import '../css/App.scss';
 import Footer from "./Footer.js";
 import Header from "./Header.js";
 import SingleMovie from './SingleMovie';

871fe1b1ed20bc558558311d1a25c0346728b7b2
Karrar Qasim <Karawr1996@gmail.com>
2023-02-09 17:12:36 -0700

    Added filter functionality

--- src/components/AllMovies.js
+++ src/components/AllMovies.js
@@ -7,7 +7,7 @@ import { getAllMovies } from '../apiCalls/';
 export default class AllMovies extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { movies: [] };
+        this.state = { movies: [], byRating:'0' };
     }
 
     componentDidMount() {
@@ -24,25 +24,42 @@ export default class AllMovies extends React.Component {
         } 
     };
 
+    handleFilterByRating = (event) => {
+        this.setState({ byRating: event.target.value})
+    };
+
+    getFilterMovies = () => {
+        const filtered = this.state.movies.filter(movie => {
+            return movie.average_rating >= this.state.byRating
+        })
+        return filtered
+    }
+
     render() {
         return (
             <div className='section--all-movies'>
                 <section className='heading--all-movies'>
                     <h2>Filter By Rating</h2>
-                    {this.state.error && <h4 className="text--error">Couldn't communicate with the server. Please try again later.</h4>}
                     <form className='star-filter' id='star-filter'>
-                        <select id='filter-movies' name='filter-movies' placeholder='filter by rating'>
-                            <option value='one-star'>⭐️</option>
-                            <option value='two-stars'>⭐️⭐️</option>
-                            <option value='three-stars'>⭐️⭐️⭐️</option>
-                            <option value='four-stars'>⭐️⭐️⭐️⭐️</option>
-                            <option value='five-stars'>⭐️⭐️⭐️⭐️⭐️</option>
-                            <option value='six-stars'>⭐️⭐️⭐️⭐️⭐️⭐️</option>
-                            <option value='seven-stars'>⭐️⭐️⭐️⭐️⭐️⭐️⭐️</option>
+                        <select 
+                        onChange={this.handleFilterByRating}
+                        id='filter-movies' 
+                        name='filter-movies' 
+                        placeholder='filter by rating'
+                        >
+                            <option value='0'>Rating</option>
+                            <option value='1'>⭐️</option>
+                            <option value='2'>⭐️⭐️</option>
+                            <option value='3'>⭐️⭐️⭐️</option>
+                            <option value='4'>⭐️⭐️⭐️⭐️</option>
+                            <option value='5'>⭐️⭐️⭐️⭐️⭐️</option>
+                            <option value='6'>⭐️⭐️⭐️⭐️⭐️⭐️</option>
+                            <option value='7'>⭐️⭐️⭐️⭐️⭐️⭐️⭐️</option>
                         </select>
                     </form>
                 </section>
-                <MovieCardsContainer movies={this.state.movies} selectMovie={this.props.selectMovie}/>
+                {this.state.error && <h4 className='error'>{this.state.error}</h4>}
+                <MovieCardsContainer movies={this.getFilterMovies()} selectMovie={this.props.selectMovie}/>
             </div>        
         );
     }

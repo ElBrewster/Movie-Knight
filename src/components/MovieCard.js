1ba4c276316a7b88dd6d6290e65df99165cc6bd8
El Brewster <brewsterel3@gmail.com>
2023-02-13 10:31:43 -0800

    Add proptypes testing for MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "../css/SingleMovie.scss";
 import {Link} from "react-router-dom";
 
@@ -13,3 +14,9 @@ export default function MovieCard ({ posterPath, id, title }) {
         </div>
     );
 }
+
+MovieCard.propTypes = {
+    posterPath: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired
+};
